e179261a5be51da989c25444bc5e914b41067d75
Igor Budasov <igor.budasov@gmail.com>
2024-01-08 17:15:42 +0100

    installing prometheus metrics bundle to expose metrics

--- application/src/index.ts
+++ application/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 
+const promBundle = require('express-prom-bundle');
+const metricsMiddleware = promBundle({ includeMethod: true });
+
 const app = express();
+app.use(metricsMiddleware);
+
 app.listen(8000, () => {
   console.log(`server running on http://localhost:8000`);
+});
+
+app.get('/', (req, res) => {
+  res.json({ werkt: 'ja!' });
 });
 
 app.get('/public/v1/urls', (req, res) => {
@@ -13,9 +22,6 @@ app.get('/health', (req, res) => {
   res.json({ gezond: 'ja goed!' });
 });
 
-app.get('/metrics', (req, res) => {
-  res.json({ prometheus: 'todo' });
-});
 
 app.get('/docs', (req, res) => {
   res.json({ openApi: 'todo' });
