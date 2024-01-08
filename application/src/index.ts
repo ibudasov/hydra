c64a1e6abbd019cdde024b0e4e9211078f767ad7
Igor Budasov <igor.budasov@gmail.com>
2024-01-08 13:23:41 +0100

    some basic routes

--- application/src/index.ts
+++ application/src/index.ts
@@ -4,3 +4,19 @@ const app = express();
 app.listen(8000, () => {
   console.log(`server running on http://localhost:8000`);
 });
+
+app.get('/public/v1/urls', (req, res) => {
+  res.json({ message: 'api running' });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ gezond: 'ja goed!' });
+});
+
+app.get('/metrics', (req, res) => {
+  res.json({ prometheus: 'todo' });
+});
+
+app.get('/docs', (req, res) => {
+  res.json({ openApi: 'todo' });
+});
